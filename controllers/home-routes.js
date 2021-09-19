793d4db2d4f2b5ffc217dda82c4a7e34d6b30dfa
Katharine C Humble <katharinechumble@gmail.com>
2021-09-19 11:30:32 -0400

    updated home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,16 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Blog, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
-  Post.findAll({
+  Blog.findAll({
     attributes: [
       'id',
-      'post_url',
       'title',
+      'content',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -28,8 +27,8 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+    .then(dbBlogData => {
+      const posts = dbBlogData.map(post => post.get({ plain: true }));
 
       res.render('homepage', {
         posts,
@@ -42,23 +41,22 @@ router.get('/', (req, res) => {
     });
 });
 
-// get single post
-router.get('/post/:id', (req, res) => {
+// get single blog
+router.get('/blogs/:id', (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id
     },
     attributes: [
       'id',
-      'post_url',
       'title',
+      'content',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
         include: {
           model: User,
           attributes: ['username']
@@ -70,13 +68,13 @@ router.get('/post/:id', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+    .then(dbBlogData => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: 'No blog found with this id' });
         return;
       }
 
-      const post = dbPostData.get({ plain: true });
+      const post = dbBlogData.get({ plain: true });
 
       res.render('single-post', {
         post,
