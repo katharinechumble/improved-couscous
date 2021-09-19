8c9f8f5b91a0ed5b7e61314af9a096c99c2a840b
Katharine C Humble <katharinechumble@gmail.com>
2021-09-19 13:01:28 -0400

    updated models

--- models/Blog.js
+++ models/Blog.js
@@ -1,14 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+// create Blog model
+class Blog extends Model {}
 
-class Vote extends Model {}
-
-Vote.init(
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    content: {
+      type: DataTypes.TEXT
+      
     },
     user_id: {
       type: DataTypes.INTEGER,
@@ -16,22 +24,14 @@ Vote.init(
         model: 'user',
         key: 'id'
       }
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'post',
-        key: 'id'
-      }
     }
   },
   {
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'vote'
+    modelName: 'blog'
   }
 );
 
-module.exports = Vote;
+module.exports = Blog;
